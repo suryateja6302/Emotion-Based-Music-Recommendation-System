434bf4a957f8a3801d7702a5341010a17638ebff
Thatikonda Surya Teja <111578838+suryateja6302@users.noreply.github.com>
2024-07-10 11:14:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, Response, jsonify
+import gunicorn
+from camera import *
+
+app = Flask(__name__)
+
+headings = ("Name","Album","Artist")
+df1 = music_rec()
+df1 = df1.head(5)
+# @app.route('/')
+# def index():
+#     print(df1.to_json(orient='records'))
+#     return render_template('main.html', headings=headings, data=df1)
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+@app.route('/index2')
+def index():
+    print(df1.to_json(orient='records'))
+    return render_template('index2.html', headings=headings, data=df1)
+    # return render_template('index1.html')
+
+
+def gen(camera):
+    while True:
+        global df1
+        frame, df1 = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/t')
+def gen_table():
+    return df1.to_json(orient='records')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
